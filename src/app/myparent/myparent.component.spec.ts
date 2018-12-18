0c070f2e2ade2bcbbff8a66f9b6fd1156e939903
rochdi1 <rochdi.abdou3@gmail.com>
2018-12-18 14:43:08 +0100

    v2

--- src/app/myparent/myparent.component.spec.ts
+++ src/app/myparent/myparent.component.spec.ts
@@ -1,25 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { MyparentComponent } from './myparent.component';
+import { MychildComponent } from '../mychild/mychild.component';
 
 describe('MyparentComponent', () => {
-  let component: MyparentComponent;
-  let fixture: ComponentFixture<MyparentComponent>;
+    describe('Mocking all nested components', () => {
+    let component: MyparentComponent;
+    let fixture: ComponentFixture<MyparentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyparentComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ MyparentComponent, MychildComponent]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyparentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(() => {
+      fixture = TestBed.createComponent(MyparentComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  describe('Ignoring nested components', () => {
+    let component: MyparentComponent;
+    let fixture: ComponentFixture<MyparentComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [MyparentComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      });
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(MyparentComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
